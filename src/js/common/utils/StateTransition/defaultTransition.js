e954bd9169661fec7e4fa9db84c13d615b3ef573
Nguyen Tuan Anh <anhnt.fami@gmail.com>
2015-08-31 15:07:55 +0700

    Add phaser transition plugin wrapper

---
+++ src/js/common/utils/StateTransition/defaultTransition.js
@@ -0,0 +1,14 @@
+export default {
+    /* How long the animation should take */
+    duration: 1000,
+    /* Easing function */
+    ease: Phaser.Easing.Exponential.InOut,
+    /* Properties to tween */
+    properties: {
+        alpha: 0,
+        scale: {
+            x: 1.5,
+            y: 1.5
+        }
+    }
+}
---
+++ src/js/common/utils/StateTransition/index.js
@@ -0,0 +1,30 @@
+/**
+ * State Transition Plugin Wrapper
+ * Supports effect disabling on receiving memory warning on Cocoonjs.
+ * Wrap `to` method. The arguments includes:
+ * - opts - The options of game.stateTransition.configure, set to null to use the defaultTransition.
+ * - The rest are in the arguments of game.state.start.
+ */
+
+var defaultTransition = require('./defaultTransition');
+/**
+ * Disable transition animation on
+ * receiving memory warning on Cocoonjs
+ */
+var disableTransition = false;
+if (typeof Cocoon != 'undefined' && Cocoon.on) {
+	Cocoon.on('memorywarning', () => disableTransition = true);
+}
+
+export default class StateTransition extends Phaser.Plugin.StateTransition {
+	to(opts, ...args) {
+		if (disableTransition) {
+			/** No animation */
+			return this.game.state.start(...args);
+		} else {
+			/** Reconfigure per transition */
+			this.configure(opts || defaultTransition);
+			return super.to(...args);
+		}
+	}
+}
