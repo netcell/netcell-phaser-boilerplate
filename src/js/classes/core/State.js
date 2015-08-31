468049246923d38a298b4c83f95c0f9ebdb9f525
Nguyen Tuan Anh <anhnt.fami@gmail.com>
2015-08-31 15:08:12 +0700

    common State definition

---
+++ src/js/classes/core/State.js
@@ -0,0 +1,17 @@
+var signals = require('../../common/signals');
+/**
+ * This is a class that should be extended to create states.
+ * Remember to call super.methodName at the beginning of
+ * the method definition.
+ */
+export default class State {
+	constructor() {
+
+	}
+	create() {
+
+	}
+	shutdown() {
+
+	}
+}
